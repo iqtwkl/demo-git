f324e4c2c7763e2379a54a7bb688f1d9ac713ecc
mshandywinata <mshandywinata@upi.edu>
2024-06-05 18:18:31 +0700

    edit main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char const *argv[])
     cout << "hello world" << endl;
     cout << "mshandywinata" << endl;
     cout << "rafi nazhmi" << endl;
+    cout << "hello, world!" << endl;
 
     cout << "Hello gesssasioaioasvkaivn\n";
     return 0;
