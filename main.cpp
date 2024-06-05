867dff5d80d968a7606424fcd0e58f1e2da5fd35
mshandywinata <mshandywinata@upi.edu>
2024-06-05 17:31:28 +0700

    add newline in main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout << "hello world";
+    cout << "hello world" << endl;
+    cout << "mshandywinata" << endl;
     return 0;
 }
