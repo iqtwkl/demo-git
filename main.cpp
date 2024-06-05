d4e242926125529ef85d38ec1fe8729445c4033b
gchristiansun <167200722+gchristiansun@users.noreply.github.com>
2024-06-05 17:44:11 +0700

    nama

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ int main(int argc, char const *argv[])
     cout << "hello world" << endl;
     cout << "mshandywinata" << endl;
     cout << "rafi nazhmi" << endl;
+    cout << "gchristiansun" << endl;
     return 0;
 }
